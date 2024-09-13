0c388a0a6e24f209481a42b3b085462f702c6f1f
josel <josel@JOSE-MSI>
2024-09-13 13:46:22 +0200

    Bucles - Ejemplos while y do-while

---
+++ src/es/cursojava/inicio/bucles/EjemploBucleDoWhile.java
@@ -0,0 +1,14 @@
+package es.cursojava.inicio.bucles;
+
+public class EjemploBucleDoWhile {
+
+	public static void main(String[] args) {
+		int numero=200;
+		do {
+			System.out.println("Prueba DoWhile");
+			
+		}while (numero<100);
+		System.out.println("FIN");
+	}
+
+}
---
+++ src/es/cursojava/inicio/bucles/EjemploBucleWhile.java
@@ -0,0 +1,16 @@
+package es.cursojava.inicio.bucles;
+
+public class EjemploBucleWhile {
+
+	public static void main(String[] args) {
+		
+		int numero = 3;
+		while(7>numero) {
+			System.out.println("Prueba while");
+			numero++;
+		
+		}
+		System.out.println("FIN");
+	}
+
+}
