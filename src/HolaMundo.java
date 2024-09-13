48748101c89fbae20e476d8b7b86bc0f0c6903da
josel <josel@JOSE-MSI>
2024-09-13 12:37:52 +0200

    Cambio n

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -8,7 +8,6 @@ public class HolaMundo {
 		System.out.println("Hola Mundo4");
 		System.out.println("Hola Mundo5");
 		System.out.println("Hola Mundo6");
-		System.out.println("Hola Mundo7");
 		
 	}
 	
