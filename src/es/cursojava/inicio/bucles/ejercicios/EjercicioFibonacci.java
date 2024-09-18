a83363a90cafd8f40a9f29138e804fd35209171d
josel <josel@JOSE-MSI>
2024-09-18 09:58:31 +0200

    Ejercicio Fibonacci Modificación ejercicios tablas de multiplicar con while en vez de for

---
+++ src/es/cursojava/inicio/bucles/ejercicios/EjercicioFibonacci.java
@@ -0,0 +1,26 @@
+package es.cursojava.inicio.bucles.ejercicios;
+
+//Mostrar los 10 primeros números de la Serie de Fibonacci
+//0,1,1,2,3,5,8,13
+public class EjercicioFibonacci {
+
+	public static void main(String[] args) {
+		int num1 = 0;
+		int num2 = 1;
+		
+		System.out.print(num1 +", ");
+		System.out.print(num2 +", ");
+		
+		int contador = 0; 
+		
+		do {
+			int suma = num1 + num2;
+			System.out.print(suma +", ");
+			num1 = num2;
+			num2 = suma;
+			contador++;
+		}while(contador<98);
+
+	}
+
+}
--- src/es/cursojava/inicio/bucles/ejercicios/EjercicioTablasMultiplicar.java
+++ src/es/cursojava/inicio/bucles/ejercicios/EjercicioTablasMultiplicar.java
@@ -4,14 +4,24 @@ public class EjercicioTablasMultiplicar {
 
 	public static void main(String[] args) {
 		
-		for (int i=1 ; i<=10 ; i++) {
+//		for (int i=1 ; i<=10 ; i++) {
+//			System.out.println("Tabla de multiplicar del " + i);
+//			for (int j=1 ; j<=10 ; j++) {
+//				System.out.println(i + " X " + j + " = " + (i*j));
+//			}
+//			System.out.println();
+//		}
+
+		int i=1;
+		while(i<=10) {
 			System.out.println("Tabla de multiplicar del " + i);
 			for (int j=1 ; j<=10 ; j++) {
 				System.out.println(i + " X " + j + " = " + (i*j));
 			}
+			i++;
 			System.out.println();
 		}
-
+		
 	}
 
 }
