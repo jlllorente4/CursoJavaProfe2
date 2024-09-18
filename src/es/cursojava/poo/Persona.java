4469061f7028ed1d4c2248e1ab5bef790a8bb3f8
josel <josel@JOSE-MSI>
2024-09-18 13:57:54 +0200

    Constructores y Encapsulación

--- src/es/cursojava/poo/Persona.java
+++ src/es/cursojava/poo/Persona.java
@@ -1,15 +1,61 @@
 package es.cursojava.poo;
 
 public class Persona {
-	//Variables de instancia o atributos
-	String nombre;
-	String apellidos;
-	String dni;
-	int edad;
+	// Variables de instancia o atributos
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	private int edad;
+
+	//Sobrecarga de constructores
+//	public Persona () {
+//		
+//	}
 	
-	//Métodos
+	public Persona (String nombre, String apellidos, String dni, int edad) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dni = dni;
+		this.edad = edad;
+	}
+	
+	// Métodos Getter y Setter
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	// Métodos
 	public void correr() {
 		System.out.println(nombre + " está corriendo");
-		
+
 	}
+
 }
--- src/es/cursojava/poo/PistaAtletismo.java
+++ src/es/cursojava/poo/PistaAtletismo.java
@@ -5,19 +5,23 @@ public class PistaAtletismo {
 	public static void main(String[] args) {
 		String texto = "Hola";
 		
-		Persona persona1 = new Persona();
-		System.out.println(persona1.nombre);
-		System.out.println(persona1.edad);
-		persona1.nombre="Juan";
-		System.out.println(persona1.nombre);
+		Persona persona0 = new Persona("N1","A1","1111A",36);
+		//System.out.println(persona0.getNombre());
+		
+		Persona persona1 = new Persona("Juan","A2","222B",24);
+		//System.out.println(persona1.getNombre());
+		System.out.println(persona1.getEdad());
+		persona1.setNombre("Juan");
+		//persona1.setDni("123123D");
+		System.out.println(persona1.getNombre());
 		
 		
-		Persona persona2 = new Persona();
-		persona2.nombre="Eva";
+		Persona persona2 = new Persona("Eva","A3","333C",48);
+		//persona2.setNombre("Eva");
 		
 		
-		Persona persona3 = new Persona();
-		persona3.nombre="Loli";
+		Persona persona3 = new Persona("Loli","A4","444D",18);
+		//persona3.setNombre("Loli");
 		
 		
 		persona1.correr();
@@ -25,9 +29,9 @@ public class PistaAtletismo {
 		persona3.correr();
 		
 		Persona persona4 = persona1;
-		persona4.nombre = "Jose";
-		System.out.println(persona1.nombre);//Jose
-		System.out.println(persona4.nombre);//Jose
+		persona4.setNombre("Jose");
+		System.out.println(persona1.getNombre());//Jose
+		System.out.println(persona4.getNombre());//Jose
 		
 		
 		System.out.println("Fin");
